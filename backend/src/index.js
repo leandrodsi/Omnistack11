c414bf7508eda7fad8bbd7da55ef0d2dddaccba7
Leandro Domingos <leandrodsystems@gmail.com>
2020-03-25 16:22:46 -0300

    Backend completo

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/*
+ *  SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ *  NoSQL: MongoDB, CouchDB, etc
+ */
+
+app.listen(3333);
